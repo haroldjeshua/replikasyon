a8e9b3d722941a88eb033457a1eb3255eb8fd8b2
Harold Varde <harvardii15@gmail.com>
2023-01-06 22:50:05 +0800

    ADD: pkg tw + markup init

--- travel-destination/src/App.jsx
+++ travel-destination/src/App.jsx
@@ -1,34 +1,35 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
-
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <div className="layout">
+      <header className="header">
+        <div className="header-left">
+          <h4>Travel</h4>
+        </div>
+        <div className="header-center">
+          <h6>Adventure Awaits</h6>
+        </div>
+        <div className="header-right">
+          <ul>
+            <li>About</li>
+            <li>Contact</li>
+          </ul>
+        </div>
+      </header>
+
+      <section className="hero">
+        <div className="hero-text">
+          <h1 className="display-1">Discover the World, One Trip at a Time</h1>
+          <p className="subtitle">
+            From mountain retreats to tropical paradises, we'll help you plan
+            the trip of a lifetime
+          </p>
+
+          <button className="btn">Explore</button>
+        </div>
+        <div className="hero-slider"></div>
+      </section>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
